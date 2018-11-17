2e51c9681722e9849ed2a37fef50bb49281762b3
Salman Qureshi <er.salmanqureshi@gmail.com>
2018-11-17 19:52:04 +0530

    Network

---
+++ PopularArticles/PopularArticles/Model/Article.swift
@@ -0,0 +1,13 @@
+//
+//  Article.swift
+//  PopularArticles
+//
+//  Created by Salman Qureshi on 11/17/18.
+//  Copyright © 2018 Salman Qureshi. All rights reserved.
+//
+
+import Foundation
+
+class Article {
+    
+}
---
+++ PopularArticles/PopularArticles/Network/ArticlesAPI.swift
@@ -0,0 +1,63 @@
+//
+//  ArticlesAPI.swift
+//  PopularArticles
+//
+//  Created by Salman Qureshi on 11/17/18.
+//  Copyright © 2018 Salman Qureshi. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+enum ArtlcesAPI {
+    case mostViewed
+}
+
+extension ArtlcesAPI: TargetType {
+    var headers: [String : String]? {
+        return ["Content-type" : "application/json"]
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var baseURL: URL {
+        guard let url = URL(string: API.baseUrl) else {
+            fatalError("url not configured")
+        }
+        return url
+    }
+    
+    var path: String {
+        switch self {
+        case .mostViewed:
+            return "mostpopular/v2/mostviewed/all-sections/7.json?"
+        }
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var parameterEncoding: ParameterEncoding {
+        switch self {
+        case .mostViewed:
+            return URLEncoding.queryString
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .mostViewed:
+            return .requestParameters(parameters: ["api_key": API.apiKey], encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var parameters: [String : Any]? {
+        switch self {
+        case .mostViewed:
+            return ["api_key=": API.apiKey]
+        }
+    }
+}
