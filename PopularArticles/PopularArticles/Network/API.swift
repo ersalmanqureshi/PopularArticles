6141f372d8135548981ca4abfba2d991afc6b072
Salman Qureshi <er.salmanqureshi@gmail.com>
2018-11-17 20:08:03 +0530

    Api service

--- PopularArticles/PopularArticles/Network/API.swift
+++ PopularArticles/PopularArticles/Network/API.swift
@@ -18,8 +18,20 @@ struct API {
     static let baseUrl = "http://api.nytimes.com/svc/"
     static let provider = MoyaProvider<ArtlcesAPI>(plugins: [NetworkLoggerPlugin(verbose: true)])
     
-    static func getMostPopularArticles(completion: @escaping (([String: Any]) -> ())) {
-        
+    static func getMostPopularArticles(completion: @escaping (([Article]) -> ())) {
+        provider.request(.mostViewed) { result in
+            switch result {
+            case let .success(response):
+                do {
+                    let results = try JSONDecoder().decode(ArticleResults.self, from: response.data)
+                    completion(results.articles)
+                } catch let err{
+                    print(err)
+                }
+            case let .failure(error):
+                print(error)
+            }
+        }
     }
     
 }
