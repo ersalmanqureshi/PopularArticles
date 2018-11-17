d8ec36d96c9f22eb7c51887309368c4bdd389f9d
Salman Qureshi <er.salmanqureshi@gmail.com>
2018-11-18 01:02:34 +0530

    Code cleanup

--- PopularArticles/PopularArticles/Network/API.swift
+++ PopularArticles/PopularArticles/Network/API.swift
@@ -16,7 +16,7 @@ import Moya
 struct API {
     static let apiKey = "b6c94499c2dd459d9aa72d581385d951"
     static let baseUrl = "http://api.nytimes.com/svc/"
-    static let provider = MoyaProvider<ArtlcesAPI>(plugins: [NetworkLoggerPlugin(verbose: true)])
+    static let provider = MoyaProvider<ArtlcesAPI>()
     
     static func getMostPopularArticles(completion: @escaping (([Article]) -> ())) {
         provider.request(.mostViewed) { result in
--- PopularArticles/PopularArticlesTests/PopularArticlesTests.swift
+++ PopularArticles/PopularArticlesTests/PopularArticlesTests.swift
@@ -10,7 +10,7 @@ import XCTest
 @testable import PopularArticles
 
 class PopularArticlesTests: XCTestCase {
-
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
