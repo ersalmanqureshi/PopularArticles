98652fed1a46695cf562069f60b087fab99d92db
Salman Qureshi <er.salmanqureshi@gmail.com>
2018-11-17 18:49:39 +0530

    Listing and detail controller

--- PopularArticles/PopularArticles/Controller/ArticlesVC.swift
+++ PopularArticles/PopularArticles/Controller/ArticlesVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ArticlesVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ PopularArticles/PopularArticles/View/ArticlesCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticlesCell.swift
+//  PopularArticles
+//
+//  Created by Salman Qureshi on 11/17/18.
+//  Copyright © 2018 Salman Qureshi. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
